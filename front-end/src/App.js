9b9de15613e94433ee510c6bac9c4c58624236e1
roger melo <meloroger059@gmail.com>
2019-07-26 22:31:50 +0000

    finished up front-end and prepared for deploy

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import logo from './logo.png';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql'
+  uri: '/graphql'
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='container'>
-        <img
-          src={logo}
-          alt='Space X'
-          style={{ width: 300, display: 'block', margin: 'auto' }}
-        />
-        <Launches />
-      </div>
+      <Router>
+        <div className='container'>
+          <img
+            src={logo}
+            alt='Space X'
+            style={{ width: 300, display: 'block', margin: 'auto' }}
+          />
+          <Route exact path='/' component={Launches} />
+          <Route exact path='/launch/:flight_number' component={Launch} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
