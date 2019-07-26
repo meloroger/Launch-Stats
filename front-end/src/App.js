06783ba6a9f0c99377b148851e3386a15c1121ad
roger melo <meloroger059@gmail.com>
2019-07-26 17:52:26 +0000

    basic layout defined. query setup for launch and rocket details. concurrently setup «npm run dev». Launches.js showing list of launches.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './App.css';
+import logo from './logo.png';
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className='container'>
+        <img
+          src={logo}
+          alt='Space X'
+          style={{ width: 300, display: 'block', margin: 'auto' }}
+        />
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
